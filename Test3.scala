46c978e387714652ce2bac7ab0457bb84c4e77e7
RajithaGayashan <rajithagayashan95@gmail.com>
2019-06-06 08:08:21 -1200

    Total of two numbers

---
+++ Test3.scala
@@ -0,0 +1,9 @@
+object Test3{
+      def main(args:Array[String]){
+        var number1:Int=10;
+        var number2:Int=20;
+        var sum=number1+number2;
+        println(sum);
+      }
+
+}
---
+++ Test4.scala
@@ -0,0 +1,14 @@
+object Test4{
+  def main(args: Array[String]){
+      print("Enter the first number : ")
+      var number1=scala.io.StdIn.readInt();
+      
+      print("Enter the second number : ")
+      var number2=scala.io.StdIn.readInt();
+
+      print("Total : ")
+      var sum=number1+number2
+      print(sum)
+
+  }
+}
