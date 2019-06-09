7a1062e1e92ef1cd99182888972ee9f50cfb3929
RajithaGayashan <rajithagayashan95@gmail.com>
2019-06-09 02:32:40 -1200

    User input

---
+++ Test9.scala
@@ -0,0 +1,11 @@
+object Test9{
+  def main(ars:Array[String]){
+    var number=0;
+    while(number!=10){
+      print("Enter your number : ");
+      number=scala.io.StdIn.readInt();
+      
+    }
+    print("You entered number 10");
+  }
+}
